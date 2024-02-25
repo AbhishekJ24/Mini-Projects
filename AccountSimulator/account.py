a4f750c70e8380e132c92d5af0ca3e781c598d93
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-02-26 00:44:40 +0530

    Updated account.py

--- AccountSimulator/account.py
+++ AccountSimulator/account.py
@@ -4,7 +4,8 @@ import pygame
 class Account:
     def __init__(self):
         self.amount=0
-        print("ACCOUNT CREATED AT PYTHON BANK SUCCESSFULLY")
+        print("\n--------------------------------------------------------\n")
+        print("\nACCOUNT CREATED AT PYTHON BANK SUCCESSFULLY")
     
     def credit(self,creditamt):
         self.amount+=creditamt
@@ -19,14 +20,15 @@ class Account:
 
 # USING PYGAME TO LOAD A MUSIC FILE THAT PLAYS SIMULTANEOUSLY WHILE THE USER USES THE FUNCTIONALITY OF THE SCRIPT
 pygame.init()
-pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.load('/Users/gamingspectrum24/Documents/PRG/Projects/Mini Projects/AccountSimulator/music.mp3')
 pygame.mixer.music.play()
 
 # CREATING AN OBJECT FOR USER 1 OF CLASS ACCOUNT
 user1=Account()
 
 # A MENU DRIVEN APPROACH TO SIMULATE AN ATM MACHINE THAT ENABLES THE USER TO CREDIT AND DEBIT MONEY AND DISPLAY THE BALANCE AND TERMINATE IF NOT REQUIRED
-while(True):
+
+while True:
     choice=int(input("\nEnter your choice?\n1 for CREDIT\n2 for DEBIT\n3 for BALANCE DISPLAY\n4 for EXIT\n"))
     if choice==1:
         cre = int(input("AMOUNT: "))
